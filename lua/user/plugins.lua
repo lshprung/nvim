db332bc52f229f2f66a71107bfbd38f72d81a3cd
louie <louie@example.com>
2022-06-24 19:38:59 -0700

    Disabled snippet completion

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -23,6 +23,6 @@ return packer.startup(function(use)
 
 	-- snippets
 	use "L3MON4D3/LuaSnip"             --snippet engine
-	use "rafamadriz/friendly-snippets" --a bunch of snippets to use
+	--use "rafamadriz/friendly-snippets" --a bunch of snippets to use
 
 end)
