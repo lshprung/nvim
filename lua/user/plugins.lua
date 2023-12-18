a3cf83dfb6776f25937dfdf35cc1a76cc2ce9db1
louie <lshprung@tutanota.com>
2023-12-18 18:25:21 -0500

    Bump telescope.nvim version

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -41,6 +41,6 @@ return packer.startup(function(use)
 	use "nvim-treesitter/nvim-treesitter" --enable treesitter
 
 	-- Telescope
-	use { "nvim-telescope/telescope.nvim", tag = '0.1.1' }
+	use { "nvim-telescope/telescope.nvim", tag = '0.1.5' }
 
 end)
