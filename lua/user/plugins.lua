623df738a2f641f9c91cfa12f97a8f880c43e6ae
Louie Shprung <lshprung@tutanota.com>
2025-04-02 09:36:10 -0400

    Replace packer with lazy

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,46 +1,70 @@
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
-end
-
--- Install your plugins here
-return packer.startup(function(use)
-	-- My plugins here
-	use "wbthomason/packer.nvim" -- Have packer manage itself
-	--use "nvim-lua/popup.nvim"          -- An implementation of the Popup API from vim in Neovim
-	use "nvim-lua/plenary.nvim"        -- Useful lua functions used by lots of plugins
+-- list of plugins to manage
+plugin_list = {
+	--"nvim-lua/popup.nvim",          -- An implementation of the Popup API from vim in Neovim
+	"nvim-lua/plenary.nvim",        -- Useful lua functions used by lots of plugins
 
 	-- Colorschemes
-	--use "lunarvim/colorschemes"        -- Additional colorschemes
-	use "marko-cerovac/material.nvim"
+	--"lunarvim/colorschemes"         -- Additional colorschemes
+	"marko-cerovac/material.nvim",   -- Material color theme
 
 	-- Buffers
-	use 'matbme/JABS.nvim'
+	'matbme/JABS.nvim',
 
 	-- cmp plugins
-	use "hrsh7th/nvim-cmp" -- The completion plugin
-	use "hrsh7th/cmp-buffer" -- Buffer completions
-	use "hrsh7th/cmp-path" -- Path completions
-	use "hrsh7th/cmp-cmdline" -- Cmdline completions
-	use "saadparwaiz1/cmp_luasnip" -- snippet completions
-	use "hrsh7th/cmp-nvim-lsp" -- lsp completion
-	use "hrsh7th/cmp-nvim-lua" -- config completion
+	"hrsh7th/nvim-cmp",         -- The completion plugin
+	"hrsh7th/cmp-buffer",       -- Buffer completions
+	"hrsh7th/cmp-path",         -- Path completions
+	"hrsh7th/cmp-cmdline",      -- Cmdline completions
+	"saadparwaiz1/cmp_luasnip", -- snippet completions
+	"hrsh7th/cmp-nvim-lsp",     -- lsp completion
+	"hrsh7th/cmp-nvim-lua",     -- config completion
 
 	-- snippets
-	use "L3MON4D3/LuaSnip" --snippet engine
-	--use "rafamadriz/friendly-snippets" --a bunch of snippets to use
+	"L3MON4D3/LuaSnip",            -- snippet engine
+	--"rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
 	-- LSP
-	use "neovim/nvim-lspconfig" -- enable LSP
+	"neovim/nvim-lspconfig",              -- enable LSP
 	--use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-	use "williamboman/mason.nvim"
-	use "williamboman/mason-lspconfig.nvim"
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
 
 	-- TreeSitter
-	use "nvim-treesitter/nvim-treesitter" --enable treesitter
+	"nvim-treesitter/nvim-treesitter", --enable treesitter
 
 	-- Telescope
-	use { "nvim-telescope/telescope.nvim", tag = '0.1.8' }
+	{ "nvim-telescope/telescope.nvim", tag = '0.1.8' }
+}
 
-end)
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = plugin_list,
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
