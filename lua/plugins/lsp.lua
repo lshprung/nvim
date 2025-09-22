b24b391da3340a73419aa2adf04e98fde7439808
Louie Shprung <lshprung@tutanota.com>
2025-09-22 13:51:42 -0400

    Replace deprecated sign_define with diagnostic_config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,14 +15,11 @@ return {
 			Hint = "? ",
 			Info = "i "
 		}
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, {
-				text = icon,
-				texthl = hl,
-				numhl = ""
-			})
-		end
+		vim.diagnostic.config({
+			signs = {
+				text = signs,
+			},
+		})
 
 		-- Diagnostic config
 		local config = {
